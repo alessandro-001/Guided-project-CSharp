f845858ce80602e451b6f8030692d70e2407a43a
alessandro-001 <alessandrofrondini@gmail.com>
2025-02-08 19:59:16 +0000

    project completed

--- Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net9.0/Starter.AssemblyInfo.cs
+++ Guided-project-branching-looping-CSharp-main/GuidedProject/Starter/obj/Debug/net9.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f42307cd9f087137873c9926855e28d2e593bd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e83e8cf42f887caf20abb76a23de8f97a0cc597")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
